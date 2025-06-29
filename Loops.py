07a4290a30c36231131f22c914c76f36b63d042e
Wesley Santos <wesleysantossilva.2019.2@gmail.com>
2025-06-29 14:30:33 -0300

    Exercícios de Loops

---
+++ Loops.py
@@ -0,0 +1,50 @@
+#for i in range (10):
+#   print (i)
+
+#for i in range(0, 10, 3):
+#  print(i)
+
+#for i in "Wesley Santos":
+#    print (i)
+
+#string = "Hello World!"
+#for i in range(0, len(string),2):
+#    print (str(i) + "ª letra é " + string[i])
+
+#count = 0
+#while (count <= 12):
+#    print (count)
+#
+#    count += 3
+
+#while True:
+#    user = input ('Digite algo que será repetido: ')
+#    if user == "Fim":
+#        print ('Acabou!')
+#        break
+#    print (user)
+
+#fim = False
+#while fim == False:
+#    user = input ('Escreva algo que será repetido: ')
+#    if user == 'fim':
+#        print ('Fim de jogo!')
+#        fim = True
+#    else:
+#        print (user)
+
+#count = 1
+
+#while count + 1 <= 20:
+#    if count % 5 == 0:
+#        print ('Pula')
+#        count += 1
+#        continue
+#    print (count)
+#    count += 1
+
+for i in range (1,20):
+    if i % 5 == 0:
+        print ('Pula')
+        continue
+    print (i)
