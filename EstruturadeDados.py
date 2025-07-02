0b0ac5959d4b553b9ccfb1bcfd242217ac25449b
Wesley Santos <wesleysantossilva.2019.2@gmail.com>
2025-07-01 21:49:41 -0300

    Pequenos exercícios com listas, tuplas e dicionários

---
+++ EstruturadeDados.py
@@ -0,0 +1,24 @@
+# Lista Vazia
+
+list1 = []
+# ou
+list1 = list()
+
+list2 = ['hello', 'Alô', 3, 'Dinossauro', 5]
+
+list3 = ['Jhon', 'male', 20, False]
+
+print("First element in list2 : "+ list2[0])
+print("Second element in list2 : "+ list2[1])
+
+list2.insert(1,'hallo')
+list2[1]
+
+print("First element in list2 : "+ list2[0])
+print("Second element in list2 : "+ list2[1])
+
+list2.append('bye')
+print (list2)
+
+list2.remove('hello')
+print (list2)
